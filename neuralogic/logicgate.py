dbd3e70e57ada6835821df2d81abcb9db01276c2
OscarMoliina <oscar.molina.sedano@estudiantat.upc.edu>
2023-06-23 18:21:07 +0200

    documentation

--- neuralogic/logicgate.py
+++ neuralogic/logicgate.py
@@ -4,17 +4,35 @@ from itertools import product
 from neuralogic.nn.neuron import Neuron, Node, OutputNeuron
 
 class LogicGate(Node):
-    '''
-    Logic Gate: Directional weighted graph of Neurons
+    r'''
+    Logic Gate
+    ---
+    Directional weighted graph of McCulloch-Pitts Neurons
+    capable to simulate all logic gates.
+
+    Attributes:
+    - variables: int (default=2)
+    number of variables that contains the LogicGate.
+    - outputn: OutputNeuron (default=None)
+    the last neuron of the network.
+    - neurons: list (default=[])
+    a list containing all neurons in the network.
+    - inputs: list (default=[])
+    a list of Node intances, one for each variable.
+    - combinations: int (default=2**self.variables)
+    number of combinations of inputs. 
+    - structure: dict (default={})
+    the graph network structure.
     '''
     def __init__(
         self,
         variables:int = 2
     ):
+        self.variables = variables
         self.outputn: OutputNeuron = None
         self.neurons = []
-        self.combinations = None
-        self.variables = variables #inputsize
+        self.inputs = []
+        self.combinations = 2**self.variables
         self.insertInputs()
         self.structure = {tpl:[] for tpl in self.inputs}
 
@@ -24,36 +42,59 @@ class LogicGate(Node):
         return s
     
     def insertInputs(self) -> List[Node]:
-        '''
-        Inplace method that compute self.inputs, self.combinations and self.variables.
-        The first one being a List with self.variables Nodes, one for each
+        r'''
+        Called in the initialization of any LogicGate instance.
+
+        Inplace method that compute ``self.inputs``, ``self.combinations`` and ``self.variables``.
+        The first one being a List with ``self.variables`` Nodes, one for each
         variable containing a list of all the values for its variable
         in each combination.
 
-        Note that the output of each node is a list with length equal
+        This method uses the built-in function ``product()`` from ``itertools`` library for the 
+        creation of all inputs.
+
+        Note that the output of each node is a list with equal size
         to the number of combinations.
         '''
         binary = [0,1]
         inputs = list(product(binary,repeat=self.variables))
-        self.inputs = [Node(out=[i[j] for i in inputs], isinput=True) for j in range(len(inputs[0]))]
+        self.inputs = [Node(out=[i[j] for i in inputs], isinput=True) for j in range(self.variables)]
         self.combinations = len(inputs)
 
     def add(self, n):
-        '''
+        r'''
         Adds a non-connected neuron to the logic gate.
+
+        If it's an instance of OutputNeuron, the method assigns ``self.outputn`` to the
+        neuron. 
         '''
-        if isinstance(n, Neuron) and not isinstance(n, OutputNeuron):
+        if isinstance(n, Neuron):
             self.neurons.append(n)
             self.structure[n] = []
-        elif isinstance(n, OutputNeuron):
-            assert self.outputn == None, 'Can only exists one instance of OutputNeuron at the same LogicGate'
-            self.outputn = n
-            self.neurons.append(n)
-            self.structure[n] = []
+            if isinstance(n, OutputNeuron):
+                assert self.outputn == None, 'Can only exists one instance of OutputNeuron at the same LogicGate'
+                self.outputn = n
 
     def merge(self, *args:List):
-        '''
-        Combines 2 instances of LogicGate
+        r'''
+        Inplace method that connects as instances of LogicGate as the number of variables
+        that accepts the main LogicGate. The weights of the connexions are fixed and saved
+        in ``n.weigths`` for each neuron.
+
+        Its operation is basically clean the inputs of the receptive LogicGate and connect
+        each neuron that in the first layer with the OutputNeuron of each ``*args`` LogicGate.
+
+        Example - XOR building:
+        ```python
+        def XOR() -> LogicGate:
+            FINAL = AND()
+            OR_ = OR()
+            NAND_ = NAND()
+            FINAL.merge(OR_, NAND_)
+            return FINAL
+        ```
+
+        It's useful in any creation of non-basic logic gates.
         '''
         assert len(args) == self.variables, \
             f'This instance of {self.__class__} requires {self.combinations} \
@@ -63,25 +104,29 @@ class LogicGate(Node):
             if n.firstlayer:
                 n.inputs = []
                 for idx,lg in enumerate(args):
-                    self.connect(n1=lg.outputn, n2=n, w=n.weigths[idx])
+                    self.connect(n1=lg.outputn, n2=n, w=n.weights[idx])
         
         self.combinations = max([self.combinations]+[arg.combinations for arg in args])
         
     def connect(self, n1, n2, w):
-        '''
+        r'''
         Adds a weigthed connection between neuron 1 and 2
         '''
         assert isinstance(n2,Neuron), 'n2 must be an instance of Neuron.'
-        if isinstance(n1,Node) or isinstance(n1,Neuron):
+        if isinstance(n1,Node):
             self.structure[n2].append(n1)
             n2.inputs.append(n1)
-            n2.weigths.append(w)
+            n2.weights.append(w)
         else:
-            raise TypeError
+            raise TypeError('n1 must be an instance of Node.')
     
     def predict(self) -> Literal[0, 1]:
-        '''
+        r'''
         Executes the network and returns the results for all tuples in inputs
+
+        It works calling the ``compute()`` method of ``self.outputn``, which calls
+        recursively the ``compute()`` method of each Node in the network.
+
         The final result is saved in self.out.
         '''
         self.out = []
--- neuralogic/nn/neuron.py
+++ neuralogic/nn/neuron.py
@@ -1,6 +1,18 @@
 from typing import List, Tuple, Literal
 
 class Node:
+    r'''
+    Node
+    ---
+    Abstract class which englobes all the elements in the network.
+
+    Attributes:
+    - out: int|list (default=None)
+    output number (or list) of the node. It'll be list of numbers
+    if the instance represents an input node of the network.
+    - isinput: bool (default=False) 
+    True when the the instance represents an input node of the network.
+    '''
     def __init__(
         self, 
         key:int = None, 
@@ -18,15 +30,32 @@ class Node:
         return s
 
 class Neuron(Node):
+    r'''
+    Neuron
+    ---
+    Class representing a McCulloch-Pitts Perceptron. 
+    It inherits from Node.
+
+    Attributes:
+    - tau: int (default=0)
+    this number is the limit to decide the output of the neuron
+    given a list of inputs and weigths.
+    - inputs: list (default=[])
+    list of nodes from which its output is taken as input for the neuron.
+    - weights: list (default=[])
+    list of weights which connects each input to the neuron.
+    - firstlayer: bool (default=False)
+    True if the neuron belong to the first layer of the LogicGate.
+    '''
     def __init__(
         self, 
-        tau:int, 
+        tau:int = 0, 
         firstlayer:bool = False
     ) -> None:
         super().__init__()
         self.tau = tau
         self.inputs:List[Node] = []
-        self.weigths:List[int] = []
+        self.weights:List[int] = []
         self.firstlayer = firstlayer
     
     def __repr__(self) -> str:
@@ -34,15 +63,27 @@ class Neuron(Node):
         return s
 
     def compute(self,it) -> Literal[0, 1]:
+        r'''
+        Method which computes the output of the neuron.
+
+        .. math::
+        \[
+        \textbf{function: } 
+        \begin{cases}
+            1, & \text{if } \sum_{i=1}^{\text{len(inputs)}} x_i w_i \geq \tau \\
+            0, & \text{else}
+        \end{cases}
+        \]
+        '''
         for n in self.inputs:
             if not n.isinput:
                 n.compute(it=it)
-        self.out = 1 if sum([v.out[it]*w if v.isinput else v.out*w for v,w in zip(self.inputs,self.weigths)]) >= self.tau else 0
+        self.out = 1 if sum([v.out[it]*w if v.isinput else v.out*w for v,w in zip(self.inputs,self.weights)]) >= self.tau else 0
         return self.out
 
 class OutputNeuron(Neuron):
-    '''
-    Abstract class refering to the unique output neuron.
+    r'''
+    Abstract class refering to the unique output neuron. It inherit from Neuron class.
     '''
     def __init__(self, tau: int, firstlayer:bool) -> None:
         super().__init__(tau, firstlayer)
